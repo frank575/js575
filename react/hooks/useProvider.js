3b8409f94c06e700f1bafed0ad1c9a31453b4e26
吳俊緯 <wcw850710@gmail.com>
2021-03-10 21:09:00 +0800

    useProvider: jsx -> js

---
+++ react/hooks/useProvider.js
@@ -0,0 +1,24 @@
+import { createContext, useContext } from 'react'
+
+/**
+ * 創建供給者
+ * @template T
+ * @param {function(): T} providerService
+ * @return {{inject: function(): T, Provider: function({children: *}): *}}
+ */
+function useProvider(providerService) {
+	const Context = createContext(null)
+
+	const Provider = ({ children }) => (
+		<Context.Provider value={providerService()}>{children}</Context.Provider>
+	)
+
+	const inject = () => useContext(Context)
+
+	return {
+		Provider,
+		inject,
+	}
+}
+
+export default useProvider
