1f7230eefae3d00fb1e6057489d84d67e38a2ff2
吳俊緯 <wcw850710@gmail.com>
2021-03-18 18:02:59 +0800

    routerGenerator: api 調整

--- react/lib/routerGenerator.jsx
+++ react/lib/routerGenerator.jsx
@@ -1,4 +1,4 @@
-import React, { useMemo, useRef } from 'react'
+import React, { useRef } from 'react'
 import { Redirect, Route, useLocation } from 'react-router-dom'
 
 const routerGenerator = (() => {
@@ -18,7 +18,7 @@ const routerGenerator = (() => {
 		}
 	}
 
-	const recurRoutes = (prefix, routes, dataRoutes = []) => {
+	const recurRoutes = (prefix, routes, $render, dataRoutes = []) => {
 		routes.forEach(e => {
 			const { path, children, meta } = e
 			e.path = prefix + e.path
@@ -30,7 +30,7 @@ const routerGenerator = (() => {
 			if (children != null) {
 				delete e.children
 				const nextPath = path === '/' ? path : path + '/'
-				recurRoutes(nextPath, children, dataRoutes)
+				recurRoutes(nextPath, children, $render, dataRoutes)
 			}
 		})
 		return dataRoutes
@@ -39,98 +39,76 @@ const routerGenerator = (() => {
 	const RouteWrap = ({ RouteComp, jsl }) => {
 		const nextOk = useRef(true)
 		const redirectPath = useRef(undefined)
-		const jslData = useRef({})
 		const location = useLocation()
-
-		useMemo(() => {
-			const { beforeEnter, path, group } = jsl
-			const newLocation = { ...location, meta: routesMeta[path], group }
-			preRoute = curRoute
-			if (preRoute == null) {
-				preRoute = newLocation
-			}
-			curRoute = newLocation
-			if (globalBeforeEnter != null) {
-				globalBeforeEnter(curRoute, preRoute, next(nextOk, redirectPath))
-			}
-			if (beforeEnter != null) {
-				beforeEnter(curRoute, preRoute, next(nextOk, redirectPath))
-			}
-			jslData.current = {
-				from: preRoute,
-				to: curRoute,
-				meta: newLocation.meta,
-				group,
-			}
-		}, [jsl])
+		const { beforeEnter, path } = jsl
+		const newLocation = { ...location, meta: routesMeta[path] }
+		preRoute = curRoute
+		if (preRoute == null) {
+			preRoute = newLocation
+		}
+		curRoute = newLocation
+		if (globalBeforeEnter != null) {
+			globalBeforeEnter(curRoute, preRoute, next(nextOk, redirectPath))
+		}
+		if (beforeEnter != null) {
+			beforeEnter(curRoute, preRoute, next(nextOk, redirectPath))
+		}
 
 		if (redirectPath.current != null)
 			return <Redirect to={redirectPath.current} />
 		if (!nextOk.current) return <>route next can't empty</>
-		return <RouteComp jslData={jslData.current} />
+		return (
+			<RouteComp
+				jslData={{
+					from: preRoute,
+					to: curRoute,
+					meta: newLocation.meta,
+				}}
+			/>
+		)
 	}
 
-	const mapRoutes = (group, groupKey) =>
-		group[
-			groupKey
-		].routes.map(({ path, component: RouteComp, redirect, beforeEnter }) => (
-			<Route
-				key={path}
-				path={path}
-				exact
-				render={() =>
-					redirect == null ? (
-						<RouteWrap
-							RouteComp={RouteComp}
-							jsl={{ beforeEnter, path, group: groupKey }}
-						/>
-					) : (
-						<Redirect to={redirect} />
-					)
-				}
-			/>
-		))
-
 	return (routes = []) => {
-		const group = {}
-		if (Array.isArray(routes)) {
-			group.$$common = {}
-			group.$$common.routes = recurRoutes('', routes)
-		} else {
-			for (const k in routes) {
-				const item = routes[k]
-				group[k] = {}
-				group[k].render = item.render
-				group[k].routes = recurRoutes('', item.routes)
-			}
-		}
+		const _routes = recurRoutes('', routes)
 		return {
 			beforeEnter: fun => {
 				globalBeforeEnter = fun
 			},
-			create: groupKey => {
-				if (groupKey == null) {
-					const routes = []
-					for (const k in group) {
-						const { render } = group[k]
-						if (render == null) {
-							routes.push(mapRoutes(group, k))
-						} else {
-							routes.push(render(mapRoutes(group, k)))
-						}
-					}
-					return routes
-				} else {
-					const { render } = group[groupKey]
-					if (render == null) {
-						return mapRoutes(group, groupKey)
-					} else {
-						return render(mapRoutes(group, groupKey))
-					}
-				}
-			},
+			create: () =>
+				_routes.map(
+					({
+						path,
+						component: RouteComp,
+						redirect,
+						beforeEnter,
+						$render: Render,
+					}) => (
+						<Route
+							key={path}
+							path={path}
+							exact
+							render={() => {
+								if (redirect == null) {
+									const RW = (
+										<RouteWrap
+											RouteComp={RouteComp}
+											jsl={{ beforeEnter, path }}
+										/>
+									)
+									return Render == null ? RW : <Render>{RW}</Render>
+								} else {
+									return <Redirect to={redirect} />
+								}
+							}}
+						/>
+					),
+				),
 		}
 	}
 })()
+
+const renderRoutes = (render, routes) => {
+	return routes
+}
 
 export default routerGenerator
