31f05eb4b0faaa49b4233818c3f0c7d5613fca1f
吳俊緯 <wcw850710@gmail.com>
2021-03-19 15:50:28 +0800

    hooks: remove useTeleport

--- react/hooks/index.js
+++ react/hooks/index.js
@@ -2,6 +2,5 @@ import useStorageState from './useStorageState'
 import useMethods from './useMethods'
 import useLoad from './useLoad'
 import useProvider from './useProvider'
-import useTeleport from './useTeleport'
 
-export { useStorageState, useMethods, useLoad, useProvider, useTeleport }
+export { useStorageState, useMethods, useLoad, useProvider }
