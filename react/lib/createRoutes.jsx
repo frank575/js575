f81cf0502949ce6f4f56716d0fe36a8a61574d2b
吳俊緯 <wcw850710@gmail.com>
2021-03-19 13:31:58 +0800

    createRoutes: 重寫(精簡版，無 beforeEnter)

--- react/lib/createRoutes.jsx
+++ react/lib/createRoutes.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Redirect, Route, useLocation } from 'react-router-dom'
+import { Redirect, Route } from 'react-router-dom'
 
 /**
  * 初始化路由
@@ -8,128 +8,73 @@ import { Redirect, Route, useLocation } from 'react-router-dom'
  * @property {string} path
  * @property {React.ReactNode} component
  * @property {*} redirect
- * @property {function(to: *, from: *, next: function(path?: *): void): void} beforeEnter
- * @property {React.ReactNode} render
- * @property {Object} meta
  * @property {Route[]} children
  *
  * @param {Route[]} routes
- * @returns {{beforeEnter: beforeEnter, Routes: React.ReactNode[]}}
+ * @returns {{Routes: React.ReactNode[]}}
  */
 const createRoutes = routes => {
-	let globalBeforeEnter
-	const ROUTE_INFO = {
-		from: undefined,
-		to: undefined,
-		meta: undefined,
-	}
-	const ROUTES_META = {}
-
-	const next = state => {
-		state.ok = false
-		return path => {
-			if (path != null) {
-				if (state.redirectTo == null) {
-					state.redirectTo = path
-				}
-			} else {
-				state.ok = true
-			}
-		}
-	}
-
-	const recurRoutes = (prefix, routes, ParentRender, dataRoutes = []) => {
+	const recurGenerator = (
+		routes,
+		ParentComponent,
+		prefix = '',
+		redirectComps = [],
+		routeComps = [],
+	) => {
 		routes.forEach(e => {
-			const { path, children, meta, render: Render } = e
 			e.path = prefix + e.path
-			dataRoutes.push(e)
-			if (meta != null) {
-				delete e.meta
-				ROUTES_META[e.path] = meta
+			if (e.redirect != null) {
+				redirectComps.push(
+					<Redirect key={e.path} path={e.path} to={e.redirect} exact />,
+				)
 			}
-			if (ParentRender != null) {
-				if (Render != null) {
-					e.render = ({ children }) => (
-						<ParentRender>
-							<Render>{children}</Render>
-						</ParentRender>
+			if (ParentComponent != null) {
+				if (e.component != null) {
+					const Component = e.component
+					e.component = props => (
+						<ParentComponent {...props}>
+							<Component {...props}>{props.children}</Component>
+						</ParentComponent>
 					)
-				} else {
-					e.render = ParentRender
 				}
 			}
-			if (children != null) {
-				delete e.children
-				const nextPath = path === '/' ? path : path + '/'
-				recurRoutes(nextPath, children, Render, dataRoutes)
+			if (e.children != null) {
+				const nextPath = e.path === '/' ? e.path : e.path + '/'
+				recurGenerator(
+					e.children,
+					e.component,
+					nextPath,
+					redirectComps,
+					routeComps,
+				)
+			} else {
+				if (e.component) {
+					routeComps.push(
+						<Route key={e.path} path={e.path} exact component={e.component} />,
+					)
+				}
 			}
 		})
-		return dataRoutes
-	}
-
-	const RouteWrap = ({ RouteComp, beforeEnter }) => {
-		const location = useLocation()
-		const condState = {
-			ok: true,
-			redirectTo: undefined,
-		}
-		ROUTE_INFO.meta = ROUTES_META[location.pathname]
-		ROUTE_INFO.to = location
-		if (ROUTE_INFO.from == null) {
-			ROUTE_INFO.from = location
-		}
-		if (globalBeforeEnter != null) {
-			globalBeforeEnter(ROUTE_INFO.to, ROUTE_INFO.from, next(condState))
-		}
-		if (beforeEnter != null) {
-			beforeEnter(ROUTE_INFO.to, ROUTE_INFO.from, next(condState))
-		}
-		if (condState.redirectTo != null)
-			return <Redirect to={condState.redirectTo} />
-		if (!condState.ok) return <>route next can't empty</>
-		return <RouteComp jslLocation={ROUTE_INFO} />
-	}
-
-	const init = (routes = []) =>
-		recurRoutes('', routes).map(
-			({
-				path,
-				component: RouteComp,
-				redirect,
-				beforeEnter,
-				render: Render,
-			}) => (
-				<Route
-					key={path}
-					path={path}
-					exact
-					render={() => {
-						if (redirect == null) {
-							const RW = (
-								<RouteWrap RouteComp={RouteComp} beforeEnter={beforeEnter} />
-							)
-							ROUTE_INFO.from = ROUTE_INFO.to
-							return Render == null ? RW : <Render>{RW}</Render>
-						} else {
-							return <Redirect to={redirect} />
-						}
-					}}
-				/>
-			),
-		)
-
-	/**
-	 * 全局 beforeEnter
-	 * @param {function(to: *, from: *, next: function(path?: *): void): void} fun
-	 */
-	const beforeEnter = fun => {
-		globalBeforeEnter = fun
+		return redirectComps.concat(routeComps)
 	}
 
 	return {
-		Routes: init(routes),
-		beforeEnter,
+		Routes: recurGenerator(routes),
 	}
 }
 
+/*
+	編譯結果
+	<Redirect path={'/dash'} to={'/dash/example'} exact />
+	<Redirect path={'/dash/example'} to={'/dash/example/example'} exact />
+	<Route path={'/dash/example/example'}>
+		<div>123</div>
+		<Example />
+	</Route>
+	<Route path={'/dash/example2'}>
+		<div>123</div>
+		<div>456</div>
+		<Example />
+	</Route>
+ */
 export default createRoutes
