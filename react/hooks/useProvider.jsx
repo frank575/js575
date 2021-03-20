1b1724b786e9320c3b1bb8b08c74003eed8af7df
吳俊緯 <wcw850710@gmail.com>
2021-03-20 14:12:07 +0800

    *.jsx: import React 測試代碼

--- react/hooks/useProvider.jsx
+++ react/hooks/useProvider.jsx
@@ -1,7 +1,9 @@
 import _React, { createContext, useContext } from 'react'
 
 if (process.env.NODE_ENV === 'production') {
-	React = _React
+	try {
+		React = _React
+	} catch {}
 }
 
 /**
--- react/lib/createRoutes.jsx
+++ react/lib/createRoutes.jsx
@@ -2,7 +2,9 @@ import _React from 'react'
 import { Redirect, Route, Switch } from 'react-router-dom'
 
 if (process.env.NODE_ENV === 'production') {
-	React = _React
+	try {
+		React = _React
+	} catch {}
 }
 
 /**
