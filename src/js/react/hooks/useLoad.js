87056e4b4cddfdcf8671586076ad4b0d400038fa
吳俊緯 <wcw850710@gmail.com>
2021-03-25 15:23:37 +0800

    feat: useLoad API 改寫

--- src/js/react/hooks/useLoad.js
+++ src/js/react/hooks/useLoad.js
@@ -1,86 +1,25 @@
 import {
 	useCallback,
-	useEffect,
-	useMemo,
-	useRef,
 	useState,
-	MutableRefObject,
 } from 'react'
 
 /**
- * 加載用鉤子
- * @template T
- * @param {Promise<*> | T} promiseFun
- * @param {{append?: (HTMLElement | MutableRefObject<HTMLElement>), run?: (string | boolean | Array.<string, ...*>)}} [options= { run: 'run' }] options
- * @returns {{error: string, pending: Object.<keyof T, boolean>, loading: boolean, exec: (T & { run: function(...args): Promise<*> })}}
+ * loading 鉤子
+ * @returns {[{loading: boolean} & Object.<string, boolean | undefined>, (function(asyncFun: Promise<void>, keys?: string[]): Promise<void>)]}
  */
-export const useLoad = (promiseFun, options) => {
-	const isFun = useMemo(() => typeof promiseFun === 'function', [promiseFun])
-	const run = useRef(options?.run ?? 'run')
-	const append = useRef(options?.append)
-	const throttle = useRef({})
-	const [state, setState] = useState({
-		error: undefined,
-		pending: {},
-		loading: false,
-	})
-
-	const pFun = useCallback(async (fun, key, ...args) => {
-		if (throttle.current[key]) {
-			return
-		}
-		throttle.current[key] = true
-
-		try {
-			setState({ error: undefined, pending: throttle.current, loading: true })
-			const result = await fun.call(promiseFun, ...args)
-			delete throttle.current[key]
-			setState({
-				error: undefined,
-				pending: throttle.current,
-				loading: Object.keys(throttle.current).length > 0,
-			})
-			return result
-		} catch (error) {
-			console.error(error)
-			delete throttle.current[key]
-			setState({
-				error,
-				pending: throttle.current,
-				loading: Object.keys(throttle.current).length > 0,
-			})
-		}
-	}, [])
-
-	const exec = useMemo(() => {
-		if (isFun) {
-			return { run: async (...args) => await pFun(promiseFun, 'run', ...args) }
-		} else {
-			return Object.keys(promiseFun).reduce(
-				(p, e) => (
-					(p[e] = async (...args) => await pFun(promiseFun[e], e, ...args)), p
-				),
-				{},
-			)
-		}
-	}, [promiseFun, pFun])
-
-	useEffect(() => {
-		const { current } = run
-		if (current !== false) {
-			if (Array.isArray(current)) {
-				const key = current.splice(0, 1)
-				exec[key](...current)
-			} else {
-				exec[current]()
-			}
-		}
-	}, [])
-
-	return {
-		error: state.error,
-		pending: state.pending,
-		loading: state.loading,
-		exec,
-	}
+export const useLoad = () => {
+	const [loading, setLoading] = useState({ loading: false })
+	const bindLoading = useCallback(
+		async (asyncFun, keys = []) => {
+			const isKeys = keys.length
+			const loaders = isKeys
+				? keys.reduce((p, k) => ((p[k] = true), p), {})
+				: { loading: true }
+			setLoading(loaders)
+			await asyncFun()
+			setLoading({ loading: false })
+		},
+		[setLoading],
+	)
+	return [loading, bindLoading]
 }
